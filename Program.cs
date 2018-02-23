0a0a27f60417fd2fa299a5f8bcdd869ae0317c63
Perry Reid <perry.reid@faulknermediagroup.com>
2018-02-22 17:41:40 -0800

    init repo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    static void Main()
+    {
+        // Prompt the user for minutes exercised
+        System.Console.Write("Enter how many minutes you exercised: ");
+
+        string entry = System.Console.ReadLine();
+
+        // add minutes exercised to total
+        // display total minutes exercised to the screen
+        //
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C# Lesson track Team Treehouse
+
+Using VsCode with WSL and Mono installed on Ubuntu
